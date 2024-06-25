88a4e7072a0654cc5db163c96cac9b04ecc24692
Purple-Code-sh <purple.code.sh@gmail.com>
2024-06-24 21:14:38 -0600

    adds websocket connection to local node server

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 import PropTypes from "prop-types";
 import "./style.css";
@@ -21,6 +22,9 @@ export default function Slider({ steps }) {
     setSliderValue(tempValue);
     setShowIndicator(false);
     console.log(tempValue);
+    if (socket) {
+      socket.emit("angle_1_changed", tempValue.toString());
+    }
   };
 
   const handleMouseDown = () => {
@@ -42,6 +46,25 @@ export default function Slider({ steps }) {
       setTempValue(newValue);
     }
   };
+
+  // ------------- WebSocket ----------------.
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // Conectar al servidor de Socket.IO
+    const newSocket = io("http://localhost:3000", {
+      auth: {
+        username: "chamin",
+        serverOffset: 0,
+      },
+    });
+    setSocket(newSocket);
+
+    // Limpiar la conexión al desmontar el componente
+    return () => newSocket.close();
+  }, []);
+
+  // ----------------------------------------
 
   return (
     <>
