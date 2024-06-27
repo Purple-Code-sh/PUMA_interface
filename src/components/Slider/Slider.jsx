ed5fec0deb213d0975785e0bc0d0ca708c787a54
Purple-Code-sh <purple.code.sh@gmail.com>
2024-06-26 20:11:46 -0600

    changes websocket implementation

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { io } from "socket.io-client";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 import PropTypes from "prop-types";
 import "./style.css";
@@ -12,6 +11,13 @@ export default function Slider({ steps }) {
   const [sliderValue, setSliderValue] = useState(0);
   const [tempValue, setTempValue] = useState(0);
   const [showIndicator, setShowIndicator] = useState(false);
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    // Conectar al servidor de Socket.IO
+    const socket = new WebSocket("ws://192.168.1.17:5000");
+    setWs(socket);
+  }, []);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -22,9 +28,7 @@ export default function Slider({ steps }) {
     setSliderValue(tempValue);
     setShowIndicator(false);
     console.log(tempValue);
-    if (socket) {
-      socket.emit("angle_1_changed", tempValue.toString());
-    }
+    ws.send(tempValue.toString());
   };
 
   const handleMouseDown = () => {
@@ -46,23 +50,6 @@ export default function Slider({ steps }) {
       setTempValue(newValue);
     }
   };
-
-  // ------------- WebSocket ----------------.
-  const [socket, setSocket] = useState(null);
-
-  useEffect(() => {
-    // Conectar al servidor de Socket.IO
-    const newSocket = io("http://localhost:3000", {
-      auth: {
-        username: "chamin",
-        serverOffset: 0,
-      },
-    });
-    setSocket(newSocket);
-
-    // Limpiar la conexión al desmontar el componente
-    return () => newSocket.close();
-  }, []);
 
   // ----------------------------------------
 
